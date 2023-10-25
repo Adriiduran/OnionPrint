262af009677bff77295475826a20b9ea256dc941
Adrian Duran Molero <adrian.duran@alten.es>
2023-10-25 09:46:30 +0200

    added Header and Footer component. Created enruting and added Home page

--- OnionPrint/src/App.jsx
+++ OnionPrint/src/App.jsx
@@ -1,10 +1,23 @@
-import Header from "./components/Header"
+//Dependencies
+ import { BrowserRouter, Routes, Route } from "react-router-dom";
+//Pages
+import Home from "./pages/Home.jsx";
+//Components
+import Header from "./components/Header/Header.jsx"
+import Footer from "./components/Footer/Footer.jsx"
+//Styles
 import "./App.css"
 
-export default function App () {
+export default function App() {
     return (
         <div className="App">
-            <Header/>
+            <Header />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                </Routes>
+            </BrowserRouter>
+            <Footer />
         </div>
     )
 }
