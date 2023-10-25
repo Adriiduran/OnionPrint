593935072d0d2da0dd90d5000a9e3d688ffc35b8
Adrian Duran Molero <adrian.duran@alten.es>
2023-10-25 09:43:45 +0200

    created header component

---
+++ OnionPrint/src/components/Header/Header.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import './Header.css';
+
+function Header() {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    return (
+        <header className="header">
+            <article className='desktop'>
+                <div className="main">
+                    <img src="/src/assets/logo.svg" alt="Logo" className='logo'/>
+                    <a href="#" className='delivery'>
+                        <p>ENVÍOS A DOMICILIO</p>
+                        <img src="/src/assets/deliveryIcon.png" alt="Delivery Icon Button" />
+                    </a>
+                </div>
+                <nav className="elements">
+                    <ul>
+                        <li><span className='button'>
+                            <p>REGISTRARSE</p>
+                            <img src="/src/assets/registerIcon.png" alt="User Icon Button" />
+                        </span></li>
+                        <li><span className='button primary'>
+                            <p>IDENTIFICARSE</p>
+                            <img src="/src/assets/loginIcon.png" alt="User Icon Button" />
+                        </span></li>
+                        <li><span className='button primary'>
+                            <img src="/src/assets/cartIcon.png" alt="User Icon Button" />
+                            <p>0€</p>
+                        </span></li>
+                    </ul>
+                </nav>
+            </article>
+
+            <article className='mobile'>
+                <div className="main">
+                    <img src="/src/assets/logo.svg" alt="Logo" className='logo'/>
+                    <img className='hamburguerIcon' src={`${isMobileMenuOpen ? '/src/assets/closeIcon.png' : '/src/assets/burgerMenu.png'}`} alt="Hamburguer Menu Button" onClick={toggleMobileMenu} />
+                </div>
+                <nav className={`${isMobileMenuOpen ? 'elements' : 'mobileMenuClose'}`}>
+                    <ul>
+                        <li><a href="#" className='delivery'>
+                            <p>ENVÍOS A DOMICILIO</p>
+                            <img src="/src/assets/deliveryIcon.png" alt="Delivery Icon Button" />
+                        </a></li>
+                        <li><span className='button'>
+                            <p>REGISTRARSE</p>
+                            <img src="/src/assets/registerIcon.png" alt="User Icon Button" />
+                        </span></li>
+                        <li><span className='button primary'>
+                            <p>IDENTIFICARSE</p>
+                            <img src="/src/assets/loginIcon.png" alt="User Icon Button" />
+                        </span></li>
+                        <li><span className='button primary'>
+                            <img src="/src/assets/cartIcon.png" alt="User Icon Button" />
+                            <p>0€</p>
+                        </span></li>
+                    </ul>
+                </nav>
+            </article>
+        </header>
+    );
+}
+
+export default Header;
