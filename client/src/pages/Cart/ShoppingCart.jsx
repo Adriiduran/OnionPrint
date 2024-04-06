32bd575fc5bcaf089549e5e2af0b20aaedbeeb1a
Adriiduran <adriiiduraan@gmail.com>
2024-04-06 23:18:22 +0200

    fix: fixed free shipping label

--- client/src/pages/Cart/ShoppingCart.jsx
+++ client/src/pages/Cart/ShoppingCart.jsx
@@ -256,8 +256,8 @@ function ShoppingCart() {
                         {finalShoppingCartPreferences.items.length != 0 && (
                             <div className='shoppingCartInfoSubtotalDetails'>
                                 <span>Gastos de envío:</span>
-                                <span style={finalShoppingCartPreferences.finalPrice > 50 ? { color: 'green' } : {}}>
-                                    {finalShoppingCartPreferences.finalPrice > 50 ? ('GRATIS') : ('3.9€')}
+                                <span style={finalShoppingCartPreferences.itemsPrice > 50 ? { color: 'green' } : {}}>
+                                    {finalShoppingCartPreferences.itemsPrice > 50 ? ('GRATIS') : ('3.9€')}
                                 </span>
                             </div>
                         )}
